33fe368972bf26ecd1c994ceefae511c69829a9d
yuruhi <yuruhiya@gmail.com>
2021-05-31 18:49:56 +0900

    fix problem 2

--- code/2_1.cr
+++
@@ -1,7 +0,0 @@
-n = read_line.to_i
-puts ['(', ')'].repeated_permutations(n).select { |s|
-  a = s.reduce([0]) { |acc, c|
-    acc << acc[-1] + (c == '(' ? 1 : -1)
-  }
-  a[-1] == 0 && a.all? { |i| i >= 0 }
-}.join('\n', &.join)
