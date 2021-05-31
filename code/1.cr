7f7b9c03df05fcb5c8dd3aa5147429aefd2b99b4
yuruhi <yuruhiya@gmail.com>
2021-05-31 18:12:12 +0900

    add problem 1

---
+++ code/1.cr
@@ -0,0 +1,13 @@
+n, l = read_line.split.map(&.to_i)
+k = read_line.to_i
+a = read_line.split.map(&.to_i) + [l]
+puts (1..10**9).bsearch { |len|
+  last, cnt = 0, 0
+  a.each do |i|
+    if i - last >= len
+      cnt += 1
+      last = i
+    end
+  end
+  cnt < k + 1
+}.not_nil! - 1
