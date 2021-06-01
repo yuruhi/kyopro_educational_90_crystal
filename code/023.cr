1c2ed471a2db7a619fae63d54e84de75b08c32ca
yuruhi <yuruhiya@gmail.com>
2021-06-01 21:39:32 +0900

    refactoring 023.cr

--- code/023.cr
+++ code/023.cr
@@ -1,149 +1,3 @@
-struct Mint
-  MOD = 1000000007i64
-
-  def self.zero
-    new
-  end
-
-  def self.raw(value : Int64)
-    result = new
-    result.value = value
-    result
-  end
-
-  @value : Int64
-
-  def initialize
-    @value = 0i64
-  end
-
-  def initialize(value)
-    @value = value.to_i64 % MOD
-  end
-
-  def initialize(m : self)
-    @value = m.value
-  end
-
-  protected def value=(value : Int64)
-    @value = value
-  end
-
-  getter value : Int64
-
-  def + : self
-    self
-  end
-
-  def - : self
-    Mint.raw(value != 0 ? MOD &- value : 0i64)
-  end
-
-  def +(v)
-    self + v.to_m
-  end
-
-  def +(m : self)
-    x = value &+ m.value
-    x &-= MOD if x >= MOD
-    Mint.raw(x)
-  end
-
-  def -(v)
-    self - v.to_m
-  end
-
-  def -(m : self)
-    x = value &- m.value
-    x &+= MOD if x < 0
-    Mint.raw(x)
-  end
-
-  def *(v)
-    self * v.to_m
-  end
-
-  def *(m : self)
-    Mint.new(value &* m.value)
-  end
-
-  def /(v)
-    self / v.to_m
-  end
-
-  def /(m : self)
-    raise DivisionByZeroError.new if m.value == 0
-    a, b, u, v = m.to_i64, MOD, 1i64, 0i64
-    while b != 0
-      t = a // b
-      a &-= t &* b
-      a, b = b, a
-      u &-= t &* v
-      u, v = v, u
-    end
-    Mint.new(value &* u)
-  end
-
-  def //(v)
-    self / v
-  end
-
-  def **(exponent : Int)
-    t, res = self, Mint.raw(1i64)
-    while exponent > 0
-      res *= t if exponent & 1 == 1
-      t *= t
-      exponent >>= 1
-    end
-    res
-  end
-
-  def ==(m : self)
-    value == m.value
-  end
-
-  def ==(m : Int)
-    raise NotImplementedError.new("==")
-  end
-
-  def succ
-    Mint.raw(value != MOD &- 1 ? value &+ 1 : 0i64)
-  end
-
-  def pred
-    Mint.raw(value != 0 ? value &- 1 : MOD &- 1)
-  end
-
-  def abs
-    self
-  end
-
-  def to_i64 : Int64
-    value
-  end
-
-  delegate to_s, to: @value
-  delegate inspect, to: @value
-end
-
-struct Int
-  {% for op in %w[+ - * / //] %}
-      def {{op.id}}(value : Mint)
-        to_m {{op.id}} value
-      end
-  {% end %}
-
-  def to_m : Mint
-    Mint.new(self)
-  end
-end
-
-class String
-  def to_m : Mint
-    Mint.new(self)
-  end
-end
-
 h, w = read_line.split.map(&.to_i)
 grid = (1..h).map { read_line }
 
@@ -160,24 +14,24 @@ hash = a.zip(0..).to_h
 next1 = a.map { |bit| hash[bit >> 1] }
 next2 = a.map { |bit| hash[bit >> 1 | 1 << w]? }
 
-dp = [Mint.zero] * m
-dp[0] = 1.to_m
+MOD = 10**9 + 7
+dp = [1] + [0] * m.pred
 (0...h).each do |i|
   (0...w).each do |j|
-    dp2 = [Mint.zero] * m
+    dp2 = [0] * m
     (0...m).each do |k|
-      next if dp[k] == Mint.zero
+      next if dp[k] == 0
       bit = a[k]
-      dp2[next1[k]] += dp[k]
-      next if grid[i][j] == '#'
-      unless (i != 0 && j != 0 && bit.bit(0) == 1) ||
+      dp2[n1 = next1[k]] = (dp2[n1] + dp[k]) % MOD
+      unless grid[i][j] == '#' ||
+             (i != 0 && j != 0 && bit.bit(0) == 1) ||
              (i != 0 && bit.bit(1) == 1) ||
              (i != 0 && j != w - 1 && bit.bit(2) == 1) ||
              (j != 0 && bit.bit(w) == 1)
-        dp2[next2[k].not_nil!] += dp[k]
+        dp2[n2 = next2[k].not_nil!] = (dp2[n2] + dp[k]) % MOD
       end
     end
     dp = dp2
   end
 end
-puts dp.sum
+puts dp.reduce(0) { |sum, x| (sum + x) % MOD }
