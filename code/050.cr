8f31a2a519a9d8a5643d3f9c80a2d8690a4689c8
yuruhi <yuruhiya@gmail.com>
2021-06-05 22:08:51 +0900

    add problem 50

---
+++ code/050.cr
@@ -0,0 +1,10 @@
+MOD = 10**9 + 7
+n, l = read_line.split.map(&.to_i)
+dp = Array.new(n + 1, 0i64)
+dp[0] = 1i64
+(1..n).each do |i|
+  dp[i] += dp[i - 1]
+  dp[i] += dp[i - l] if i - l >= 0
+	dp[i] %= MOD
+end
+puts dp[n]
