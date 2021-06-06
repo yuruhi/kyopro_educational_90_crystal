4f1db1e3e4bf302425f63d98fa01a51a4823e2e2
yuruhi <yuruhiya@gmail.com>
2021-06-06 09:10:43 +0900

    add problem 46

---
+++ code/046.cr
@@ -0,0 +1,11 @@
+n = read_line.to_i
+a = (1..3).map {
+  b = read_line.split.map(&.to_i.%(46))
+  (0...46).map { |i| b.count(i) }
+}
+mod46 = (0...46).to_a
+puts Array.product(mod46, mod46, mod46).select { |indices|
+  indices.sum % 46 == 0
+}.sum { |indices|
+  a.zip(indices).product { |(b, i)| b[i].to_i64 }
+}
