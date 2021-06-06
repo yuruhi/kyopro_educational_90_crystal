fe84906e98c1b70f9384f9b824eb7c5048a34326
yuruhi <yuruhiya@gmail.com>
2021-06-06 17:18:38 +0900

    fix problem 36

--- code/036.cr
+++ code/036.cr
@@ -3,9 +3,9 @@ x, y = (1..n).map {
   a, b = read_line.split.map(&.to_i64)
   {a - b, a + b}
 }.transpose
-xx, yy = x.minmax, y.minmax
+xy = x.minmax + y.minmax
 
 q.times do
   i = read_line.to_i - 1
-  puts [x[i], x[i], y[i], y[i]].zip(xx + yy).max_of { |a, b| (a - b).abs }
+  puts [x[i], x[i], y[i], y[i]].zip(xy).max_of { |a, b| (a - b).abs }
 end
