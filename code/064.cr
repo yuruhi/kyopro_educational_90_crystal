6074b3fbd6d9e8209044f6b3f4f9ef18765c0c6f
yuruhi <yuruhiya@gmail.com>
2021-06-14 14:01:05 +0900

    refactoring 064.cr

--- code/064.cr
+++ code/064.cr
@@ -3,11 +3,11 @@ a = read_line.split.map(&.to_i64)
 d = a.each_cons(2).map { |(x, y)| x - y }.to_a
 ans = d.sum(&.abs)
 q.times do
-  l, r, v = read_line.split.try { |(l, r, v)| {l.to_i - 1, r.to_i, v.to_i64} }
-  if l != 0
-    ans -= d[l - 1].abs
+  l, r, v = read_line.split.map(&.to_i)
+  if l != 1
+    ans -= d[l - 2].abs
     d[l - 1] -= v
-    ans += d[l - 1].abs
+    ans += d[l - 2].abs
   end
   if r != n
     ans -= d[r - 1].abs
